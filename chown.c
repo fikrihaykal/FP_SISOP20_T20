f88a9075d3aaaaeba2b0637209aadc34e28bdc92
Fikri Haykal <fhaykal24@gmail.com>
2020-05-07 19:37:41 +0700

    Tetep setor lib chown.c

--- chown.c
+++ chown.c
@@ -1,8 +1,20 @@
-#include "fcntl.h"
+#ifdef CS333_P5
 #include "types.h"
-#include "stat.h"
 #include "user.h"
+#include "fcntl.h"
+#include "stat.h"
 
 int main(int argc, char *argv[]){
-    // Masih belum untuk chown.c
+    if(argc != 4){
+        printf(1, "chown fail : chown [owner] [group] [filename]\n");
+        exit();
+    } else{
+        if((chown(argv[3], argv[1]) && chgrp(argv[3], argv[2])) < 0){
+            printf(1, "chown fail : chown [owner] [group] [filename]\n");
+            exit();
+        } else{
+            printf(1, "chown : update %s owner and group\n", argv[3])
+        }
+    }
+    exit();
 }
