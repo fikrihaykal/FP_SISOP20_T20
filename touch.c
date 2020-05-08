d1c57cd57c74d3a9c4fd1d599cbe66df8b897de8
Fikri Haykal <fhaykal24@gmail.com>
2020-05-08 09:37:44 +0700

    Ganti huruf pertama

--- touch.c
+++ touch.c
@@ -4,7 +4,8 @@
 #include "user.h"
 
 int main(int argc, char *argv[]){
-	int fd;
+	int fd, r;
+	char buf[1];
 
 	if(argc!=2){
 		printf(1, "touch fail : touch [filename]\n");
@@ -13,7 +14,13 @@ int main(int argc, char *argv[]){
 	if((fd = open(argv[1], O_CREATE | O_RDWR)) < 0){
 		printf(1, "touch fail : failed update timestamp: %s\n", argv[1]);
 	} else{
-		write(fd, "", 0);
+		r = read(fd, buf, sizeof(buf));
+		if(buf[1] == 'a'){
+			write(fd, "b", 1);
+		} else{
+			write(fd, "a", 1);
+		}
+		write(fd, buf, sizeof(buf));
 	}
 
 	close(fd);
