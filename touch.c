4c54a85295e8a658aa7d95f3a893029ee8f78ae8
Fikri Haykal <fhaykal24@gmail.com>
2020-05-08 16:55:02 +0700

    Fix touch.c

--- touch.c
+++ touch.c
@@ -13,8 +13,7 @@ int main(int argc, char *argv[]){
 
 	if((fd = open(argv[1], O_CREATE | O_RDWR)) < 0){
 		printf(1, "touch fail : failed update timestamp: %s\n", argv[1]);
-	} else{
-		r = read(fd, buf, sizeof(buf));
+	} else if(r = read(fd, buf, sizeof(buf))){
 		if(buf[1] == 'a'){
 			write(fd, "b", 1);
 		} else{
